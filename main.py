f640e8d639856721991ece9d40b1da54bfad9762
arthurssena <arthurss@ymail.com>
2024-01-26 11:13:09 +0200

    26.1.v5

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ data = {'Year':['2020', '2021', '2022', '2023', '2024'], 'Citations':[28, 85, 16
 df = pd.DataFrame(data)
 
 total_citations = np.sum(data['Citations'])
-fig = px.bar(df, x='Year', y='Citations', color = 'Citations', title='<b>Total = {}'.format(total_citations), 
+fig = px.bar(df, x='Year', y='Citations', color = 'Citations', 
         color_continuous_scale = [(0,'#7fffd2'), (0.5, '#00d5ff'), (1,'#008cff')],
         text_auto = False, template="plotly_white", height=290)
 
 fig.update_coloraxes(showscale=False)
-fig.update_layout(margin=dict(l=20, r=20, t=80, b=20), title_x=0.5, showlegend=False, font_color="#6d8996", title_font_color='#00abf0')
+fig.update_layout(margin=dict(l=20, r=20, t=80, b=20), title_x=0.5, showlegend=False, font_color="#6d8996", title_font_color='#00abf0',
+                        title_text ="<b>Total = {} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span style='color:#6d8996'>h-index = 11</span>".format(total_citations))
 
 fig.update_yaxes(showgrid=True, gridwidth=.5, gridcolor='#d9e8f4')
 
