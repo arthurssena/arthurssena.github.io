ebc432a5649b35728b1b44842b3d282a235a2480
arthurssena <arthurss@ymail.com>
2024-01-26 10:45:41 +0200

    26.1.v4

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import plotly.express as px
 import numpy as np
 import pandas as pd
 
-data = {'Year':['2020', '2021', '2022', '2023', '2024'], 'Citations':[28, 85, 162, 160, 7] }
+data = {'Year':['2020', '2021', '2022', '2023', '2024'], 'Citations':[28, 85, 162, 160, 8] }
 df = pd.DataFrame(data)
 
 total_citations = np.sum(data['Citations'])
 fig = px.bar(df, x='Year', y='Citations', color = 'Citations', title='<b>Total = {}'.format(total_citations), 
         color_continuous_scale = [(0,'#7fffd2'), (0.5, '#00d5ff'), (1,'#008cff')],
-        text_auto = True, template="plotly_white", height=290)
+        text_auto = False, template="plotly_white", height=290)
 
 fig.update_coloraxes(showscale=False)
 fig.update_layout(margin=dict(l=20, r=20, t=80, b=20), title_x=0.5, showlegend=False, font_color="#6d8996", title_font_color='#00abf0')
